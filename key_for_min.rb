2c8d1902f33d228dff499a70f8ef2b62cd612507
Rajvir Rai <rsr1992@gmail.com>
2018-09-11 00:15:32 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ names = {:"Raj" => 3, "Nikki" => 5, "Theo" => 4}
 def key_for_min_value(name_hash)
   smallest_key = nil 
   smallest_value = nil
-  hash.each do | a, b|
+  name_hashhash.each do | a, b|
     if smallest_value == nil || v < smallest_value
       smallest_value = a
       smallest_key = b
